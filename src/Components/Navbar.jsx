6ce130683128a74a8928584134a93b124138ea5e
Olaide Afolabi <oafolabi@icsl.lo>
2025-05-05 11:51:40 +0100

    Changed Navbar Text Size

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,27 +11,27 @@ const Navbar = () => {
       <div className="dark:bg-dark-elements text-light flex items-center justify-between bg-gray-200 px-8 py-4 dark:text-white">
         <div className="font-Nunito font-bold">
           <Link to={"/"}>
-            <h3 className="text-2xl">Where in the world?</h3>
+            <h3 className="text-xl md:text-2xl">Where in the world?</h3>
           </Link>
         </div>
         <div>
           {theme === "dark" ? (
             <div>
               <div
-                className="font-Nunito flex cursor-pointer items-center gap-2 font-bold"
+                className="font-Nunito flex cursor-pointer items-center gap-2 text-base font-bold md:text-xl"
                 onClick={() => setTheme("light")}
               >
-                <IoSunnyOutline className="text-xl" />
+                <IoSunnyOutline />
                 <p>Light Mode</p>
               </div>
             </div>
           ) : (
             <div>
               <div
-                className="font-Nunito flex cursor-pointer items-center gap-2 font-bold"
+                className="font-Nunito flex cursor-pointer items-center gap-2 text-base font-bold md:text-xl"
                 onClick={() => setTheme("dark")}
               >
-                <IoMoonOutline className="text-xl" />
+                <IoMoonOutline />
                 <p>Dark Mode</p>
               </div>
             </div>
